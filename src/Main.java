d9848a0a5fd38b353c1d31396d2105b53af6dcb4
Denis Petrovskii <denpetrovsky369@gmail.com>
2021-12-13 06:37:27 +0300

    money transfer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int currentbalance = 2_000_000_000;
+        int moneytransfer = 500_000_000;
+        int finalbalance = moneytransfer + currentbalance;
+        System.out.println(finalbalance);
     }
 }
